ada76294a2f076409c150accdea69b7e2cf87ae8
Your Name <fantastizeey@gmail.com>
2024-09-06 15:02:44 +0100

    fine tuning the mobile veiw styles for the banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -12,7 +12,7 @@ const HeroBanner = ({ heroBanner }) => {
         <h1>{heroBanner.largeText1}</h1>
         <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
 
-        <div>
+        <div className='below'>
           <Link href={`/product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
